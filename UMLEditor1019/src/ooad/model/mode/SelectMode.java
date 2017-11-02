bdaedadf77106cc1536bfc75e8be73af9dfc01d2
tim90721 <tim90721@gmail.com>
2017-11-02 16:10:48 +0800

    update 11/2

--- UMLEditor1019/src/ooad/model/mode/SelectMode.java
+++ UMLEditor1019/src/ooad/model/mode/SelectMode.java
@@ -45,6 +45,7 @@ public class SelectMode extends AbstractMode {
 					shape.setSelected(true);
 					_selectShapes.add(shape);
 					_hasSelectShape = true;
+					
 				}
 		if (_hasSelectShape)
 			return true;
